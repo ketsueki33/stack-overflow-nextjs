72ffd0b22fa49497a3073f22810ba53db26dfc84
ketsueki33 <harsh.karaiya@gmail.com>
2024-08-20 14:49:02 +0530

    create webhook for ingesting clerk users

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,8 +2,15 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+    CreateUserParams,
+    DeleteUserParams,
+    GetUserByIdParams,
+    UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
 
-export async function getUserById(params: any) {
+export async function getUserById(params: GetUserByIdParams) {
     try {
         connectToDatabase();
 
@@ -12,6 +19,57 @@ export async function getUserById(params: any) {
         const user = await User.findOne({ clerkId: userId });
 
         return user;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function createUser(userData: CreateUserParams) {
+    try {
+        connectToDatabase();
+
+        const newUser = await User.create(userData);
+
+        return newUser;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+    try {
+        connectToDatabase();
+
+        const { clerkId, updateData, path } = params;
+
+        const user = await User.findOneAndUpdate({ clerkId }, updateData);
+
+        revalidatePath(path);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+    try {
+        connectToDatabase();
+
+        const { clerkId } = params;
+
+        const user = await User.findOneAndDelete({ clerkId });
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        // TODO: add deleted user for comments, questions, answers.
+
+        const deletedUser = await User.findByIdAndDelete(user._id);
+
+        return deletedUser;
     } catch (error) {
         console.log(error);
         throw error;
